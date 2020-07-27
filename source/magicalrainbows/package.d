e8b8b01b7d2b1c34ab5d1c2bcded2c9b3a516562
Cameron Ross <elpenguino@gmail.com>
2020-07-27 20:50:37 -0300

    add a package

---
+++ source/magicalrainbows/package.d
@@ -0,0 +1,6 @@
+module magicalrainbows;
+
+public import magicalrainbows.formats;
+public import magicalrainbows.gradient;
+public import magicalrainbows.properties;
+public import magicalrainbows.raw;

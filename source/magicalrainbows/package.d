470bbd2f3b61e35e205645193f10b53bee9fa258
Cameron Ross <elpenguino@gmail.com>
2021-01-17 18:36:50 -0400

    add some palettes

--- source/magicalrainbows/package.d
+++ source/magicalrainbows/package.d
@@ -2,5 +2,6 @@ module magicalrainbows;
 
 public import magicalrainbows.formats;
 public import magicalrainbows.gradient;
+public import magicalrainbows.palette;
 public import magicalrainbows.properties;
 public import magicalrainbows.raw;

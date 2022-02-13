6bfdcf541447f1ef8a785063e0c5df0496ca6a33
Cameron Ross <elpenguino@gmail.com>
2022-02-13 16:49:47 -0400

    add png -> tiles util

---
+++ fromimage/source/app.d
@@ -0,0 +1,62 @@
+
+import std.algorithm;
+import std.array;
+import std.format;
+import std.getopt;
+import std.stdio;
+
+import arsd.png;
+import tiledump;
+import pixelatrix;
+import magicalrainbows;
+
+void main(string[] args) {
+	string palettePath;
+	TileFormat tileFormat = TileFormat.intertwined4BPP;
+	ArrangementStyle arrangementStyle;
+	SupportedFormat paletteFormat;
+	auto helpInformation = getopt(args,
+    	std.getopt.config.caseSensitive,
+		"arrangement-style|s", "Assume a specific tile arrangement style", &arrangementStyle,
+		"palette|p", "Write a palette file", &palettePath,
+		"palette-format|F", "Palette file format", &paletteFormat,
+		"tileformat|f", &tileFormat
+	);
+
+	if (helpInformation.helpWanted || (args.length < 3)) {
+		defaultGetoptPrinter(format!"Usage: %s <image> <tile file>"(args[0]), helpInformation.options);
+		return;
+	}
+	if (auto img = cast(IndexedImage)readPng(args[1])) {
+		if (((img.width % 8) != 0) || ((img.height % 8) != 0)) {
+			stderr.writeln("Error: image dimensions are not multiples of 8!");
+			return;
+		}
+		const tileWidth = img.width / 8;
+		const tileHeight = img.height / 8;
+		ubyte[8][8][] tiles;
+		tiles.reserve(tileWidth * tileHeight);
+		auto tileRows = cast(ubyte[8][])img.data;
+		auto file = File(args[2], "wb");
+		foreach (th; 0 .. tileHeight) {
+			foreach (w; 0 .. tileWidth) {
+				ubyte[8][8] tile;
+				foreach (h; 0 .. 8) {
+					tile[h] = tileRows[th * tileWidth * 8 + h * tileWidth + w];
+				}
+				tiles ~= tile;
+			}
+		}
+		const arrangement = Arrangement.generate(arrangementStyle, tiles.length, tileWidth);
+		foreach (tileAttributes; arrangement.tiles) {
+			file.rawWrite(tileData(tiles[tileAttributes.tile], tileFormat));
+		}
+		if (palettePath) {
+			auto paletteFile = File(palettePath, "wb");
+			RGBA8888[] colours = img.palette.map!(x => RGBA8888(x.r, x.g, x.b, x.a)).array;
+			foreach (colour; colours) {
+				paletteFile.rawWrite(colourToBytes(colour, paletteFormat));
+			}
+		}
+	}
+}

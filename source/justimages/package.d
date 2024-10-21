a7987025b1394895671db5743cf1f10704c34f0e
Cameron Ross <elpenguino@gmail.com>
2024-10-21 14:03:40 -0300

    add package.d

---
+++ source/justimages/package.d
@@ -0,0 +1,14 @@
+module justimages;
+
+public import justimages.apng;
+public import justimages.bmp;
+public import justimages.color;
+public import justimages.dds;
+public import justimages.ico;
+public import justimages.image;
+public import justimages.imageresize;
+public import justimages.jpeg;
+public import justimages.pcx;
+public import justimages.png;
+public import justimages.svg;
+public import justimages.targa;
